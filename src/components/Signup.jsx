cb4eb70c8a82b6760ec5afaff78cb279edb05506
belemaaa <graceitamunoala@gmail.com>
2023-11-06 20:59:21 +0000

    changes to signup api construction

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,6 +9,7 @@ const Signup = () => {
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
     const [error_message, setError_message] = useState('')
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const headers = {
@@ -29,12 +30,9 @@ const Signup = () => {
                 console.log('signup was successful')
                 navigate('/')
             }
-            else{
-                console.log('an error was encountered.')
-                setError_message('An error occurred. Please try again later.')
-            }
         }catch(error){
             console.error('errors: ', error)
+            setError_message('An error occurred. Please try again later.')
         }
     }
   return (
@@ -42,14 +40,15 @@ const Signup = () => {
         <div>
             <p className='text-green-700 text-3xl font-bold h-16'>Signup</p>
         </div>
-        <form method='POST' className='flex flex-col'>
-            {error_message && <p className='text-white text-center font-thin'>{error_message}</p>}
+        <form method='POST' className='flex flex-col' onSubmit={handleSubmit}>
+        {error_message && <p className='text-white text-center font-thin'>{error_message}</p>}
             <input
                 type='text'
                 placeholder='Username'
                 className='border-2 bg-black rounded-lg p-3 text-white w-[350px] md:w-[450px] mb-4'
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
+                required
             />
             <input
                 type='password'
@@ -57,6 +56,7 @@ const Signup = () => {
                 className='border-2 bg-black rounded-lg p-3 text-white w-[350px] md:w-[450px] mb-4'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                required
             />
             <input
                 type='email'
@@ -64,6 +64,8 @@ const Signup = () => {
                 className='border-2 bg-black rounded-lg p-3 text-white w-[350px] md:w-[450px] mb-4'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                required
+                autoComplete='on'
             />
             <input
                 type='phone_number'
@@ -71,6 +73,8 @@ const Signup = () => {
                 className='border-2 bg-black rounded-lg p-3 text-white w-[350px] md:w-[450px] mb-4'
                 value={phone_number}
                 onChange={(e) => setPhone_number(e.target.value)}
+                required
+                autoComplete='on'
             />
             <div className='flex items-center justify-center'>
                 <button type='submit' className='bg-white p-2 w-32 rounded-lg text-black mt-8 font-semibold hover:bg-green-700 duration-300'>Proceed</button>
